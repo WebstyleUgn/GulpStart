e83c6633916246cc91824e7764ae657078f9b11a
Yevhen Lakhno <webstyle.ugn@gmail.com>
2021-02-07 00:30:18 +0200

    add first plugin gulp-rm

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
-const { src, dest } = require("gulp");
+const { src, dest, task } = require("gulp"),
+      rm = require("gulp-rm");
+
+task("clean", () => {
+    return src("dist/**/*", { read: false }).pipe(rm());
+});
 
 function copy() {
     return src("src/styles/main.scss").pipe(dest("dist"));
