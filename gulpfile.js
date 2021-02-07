f7c29743bde08dbe4896e986bce99cf2002e1130
Yevhen Lakhno <webstyle.ugn@gmail.com>
2021-02-07 10:48:13 +0200

    add default task with clean and copy

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ task("clean", () => {
     return src("dist/**/*", { read: false }).pipe(rm());
 });
 
-task("copy", series("clean", () => {
+task("copy", () => {
     return src("src/styles/main.scss").pipe(dest("dist"));
-}));
+});
+
+task("default", series("clean", "copy"));
